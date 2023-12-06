184dd4a3137a9040c128ddefc18dbdb0509bcce9
joonseokchung <50360713+joonseokchung@users.noreply.github.com>
2023-12-06 20:26:45 +0900

    Update statfile.cpp

--- src/statfile.cpp
+++ src/statfile.cpp
@@ -1,87 +1,61 @@
-//statefile.cpp
-
-
-
+#include "statfile.h"
 #include <iostream>
-#include <fstream>
-#include <chrono>
-#include <string>
 
-class StatFile {
-private:
-    int uploadCount;
-    int downloadCount;
-    std::chrono::steady_clock::time_point startTime;
-    std::ofstream outputFile;  // 파일 출력 스트림
+FileUploadStats::FileUploadStats() : uploadCount(0), totalUploadTime(0), totalUploadSize(0), downloadCount(0), totalDownloadTime(0), totalDownloadSize(0) {}
 
-    // 생성자와 소멸자를 private으로 선언하여 외부에서 객체 생성 및 소멸을 방지
-    StatFile() : uploadCount(0), downloadCount(0), startTime(std::chrono::steady_clock::now()) {
-        // 파일 출력 스트림 초기화
-        outputFile.open("stats.txt", std::ios::out | std::ios::app);
+void FileUploadStats::IncrementUploadCount() {
+    uploadCount++;
+}
 
-        std::cout << "StatFile instance created." << std::endl;
-    }
+void FileUploadStats::IncrementDownloadCount() {
+    downloadCount++;
+}
 
-    ~StatFile() {
-        // 파일 출력 스트림 닫기
-        outputFile.close();
+void FileUploadStats::UpdateUploadTimeStats(long uploadTime) {
+    totalUploadTime += uploadTime;
+}
 
-        // 여기에서 파일 저장 또는 정리 로직 수행 가능
-        std::cout << "StatFile instance destroyed." << std::endl;
-    }
+void FileUploadStats::UpdateUploadSizeStats(long uploadSize) {
+    totalUploadSize += uploadSize;
+}
 
-    // 복사 생성자와 대입 연산자를 private으로 선언하여 복사를 방지
-    StatFile(const StatFile&) = delete;
-    StatFile& operator=(const StatFile&) = delete;
+void FileUploadStats::UpdateDownloadStats(long downloadTime, long downloadSize) {
+    totalDownloadTime += downloadTime;
+    totalDownloadSize += downloadSize;
+}
 
-public:
-    // 정적 메서드를 통해 유일한 인스턴스에 접근
-    static StatFile& getInstance() {
-        // 정적 지역 변수을 사용하여 최초 호출 시에만 인스턴스를 생성
-        static StatFile instance;
-        return instance;
-    }
+void FileUploadStats::PrintStats() {
+    std::cout << "Upload Stats: Count = " << uploadCount
+              << ", Total Time = " << totalUploadTime
+              << ", Total Size = " << totalUploadSize << std::endl;
 
-    // 파일 업로드 횟수 증가 및 누적 시간 업데이트
-    void uploadFile(const std::string& filePath) {
-        uploadCount++;
-        auto endTime = std::chrono::steady_clock::now();
-        auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count();
+    std::cout << "Download Stats: Count = " << downloadCount
+              << ", Total Time = " << totalDownloadTime
+              << ", Total Size = " << totalDownloadSize << std::endl;
+}
 
-        // 여기에서 파일 업로드 통계 정보 업데이트 로직 수행 가능
-        std::cout << "Uploaded file: " << filePath << ", Elapsed Time: " << elapsedTime << " ms" << std::endl;
+FileUploadStats StatFile::fileStats;
 
-        // 파일에 통계 정보 저장
-        outputFile << "Uploaded file: " << filePath << ", Elapsed Time: " << elapsedTime << " ms" << std::endl;
-    }
+void StatFile::IncrementUploadCount() {
+    fileStats.IncrementUploadCount();
+}
 
-    // 파일 다운로드 횟수 증가 및 누적 시간 업데이트
-    void downloadFile(const std::string& filePath) {
-        downloadCount++;
-        auto endTime = std::chrono::steady_clock::now();
-        auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count();
+void StatFile::IncrementDownloadCount() {
+    fileStats.IncrementDownloadCount();
+}
 
-        // 여기에서 파일 다운로드 통계 정보 업데이트 로직 수행 가능
-        std::cout << "Downloaded file: " << filePath << ", Elapsed Time: " << elapsedTime << " ms" << std::endl;
+void StatFile::UpdateUploadTimeStats(long uploadTime) {
+    fileStats.UpdateUploadTimeStats(uploadTime);
+}
 
-        // 파일에 통계 정보 저장
-        outputFile << "Downloaded file: " << filePath << ", Elapsed Time: " << elapsedTime << " ms" << std::endl;
-    }
+void StatFile::UpdateUploadSizeStats(long uploadSize) {
+    fileStats.UpdateUploadSizeStats(uploadSize);
+}
 
-    // 평균 업로드 및 다운로드 시간 출력
-    void printAverageStats() const {
-        auto endTime = std::chrono::steady_clock::now();
-        auto totalElapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count();
+void StatFile::UpdateDownloadStats(long downloadTime, long downloadSize) {
+    fileStats.UpdateDownloadStats(downloadTime, downloadSize);
+}
 
-        // 평균 시간 계산
-        double averageUploadTime = uploadCount > 0 ? static_cast<double>(totalElapsedTime) / uploadCount : 0.0;
-        double averageDownloadTime = downloadCount > 0 ? static_cast<double>(totalElapsedTime) / downloadCount : 0.0;
-
-        std::cout << "Average Upload Time: " << averageUploadTime << " ms" << std::endl;
-        std::cout << "Average Download Time: " << averageDownloadTime << " ms" << std::endl;
-
-        // 파일에 평균 통계 정보 저장
-        outputFile << "Average Upload Time: " << averageUploadTime << " ms" << std::endl;
-        outputFile << "Average Download Time: " << averageDownloadTime << " ms" << std::endl;
-    }
-};
+void StatFile::PrintStats() {
+    fileStats.PrintStats();
+}
